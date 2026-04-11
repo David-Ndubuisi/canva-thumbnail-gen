52761badc51070ad402038033ffb5a3248a0a0c6
David-Ndubuisi <131281095+David-Ndubuisi@users.noreply.github.com>
2026-04-11 02:42:05 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // These headers are REQUIRED for FFmpeg to work locally
   server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  // Added this for 'npm run preview'
+  preview: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Embedder-Policy": "require-corp",
